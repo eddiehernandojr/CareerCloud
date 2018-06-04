eef1807e6f257b9f7671b5692f8e52fdee4ef36d
Eddie Hernando Jr <edzstrock@hotmail.com>
2018-06-03 23:47:47 -0400

    Created CareerCloud.DataAccessLayer class library project and IDataRepository interface. Created CareerCloud.ADODataAccessLayer class library project, corresponding classes and implement IDataRepository interface to each class created.

---
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -0,0 +1,81 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
+    {
+        public void Add(params ApplicantEducationPoco[] items)
+        {
+            SqlConnection conn =
+                            new SqlConnection(
+                                ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString
+                                );
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            int rowsEffected = 0;
+
+            foreach (ApplicantEducationPoco poco in items)
+            {
+                cmd.CommandText = @"INSERT INTO Applicant_Educations
+                     (Id, Applicant, Major, Certificate_Diploma, Start_Date, Completion_Date, Completion_Percent) 
+                     Values
+                     (@Id, @Applicant, @Major, @Certificate_Diploma, @Start_Date, @Completion_Date, @Completion_Percent)";
+
+                cmd.Parameters.AddWithValue("@Id", poco.Id);
+                cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                cmd.Parameters.AddWithValue("@Major", poco.Major);
+                cmd.Parameters.AddWithValue("@Certificate_Diploma", poco.CertificateDiploma);
+                cmd.Parameters.AddWithValue("@Start_Date", poco.StartDate);
+                cmd.Parameters.AddWithValue("@Completion_Date", poco.CompletionDate);
+                cmd.Parameters.AddWithValue("@Completion_Percent", poco.CompletionPercent);
+
+                conn.Open();
+                rowsEffected += cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+
+        //not included
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        //not included
+        public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantEducationPoco GetSingle(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
